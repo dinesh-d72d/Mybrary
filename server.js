891dcb6b4cd2194e7b8853dfd28584f1610f37ae
dinesh-d72d <dinesh.d72d@gmail.com>
2022-08-04 20:08:28 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-if(process.env.NODE_ENV!=='production'){
+if(process.env.NODE_ENV!== 'production'){
     require('dotenv').config()
 }
 
-const express=require('express')
-const app=express()
-const expressLayouts=require('express-ejs-layouts')
-const indexRouter=require('./routes/index')
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
 
 app.set('view engine','ejs')
 app.set('layout','layouts/layout')
 
 app.use(express.static('public'))
 app.use(expressLayouts)
+app.use(express.urlencoded({ limit: '10mb', extended: false}))
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 const mongoose=require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{
